9fc8170a1355a9155ab0db8465e8e4faced2a3ce
mathbusna <matheus.busnardo@gmail.com>
2025-09-10 15:50:45 -0300

    Commit1

--- exercicio2.m
+++ exercicio2.m
@@ -1,6 +1,6 @@
 function [blendA, blendB] = exercicio2(NG)
 
-% NG: numero do grupo
+% NG: 1
 
 % nao alterar: inicio
 es = 1;
@@ -10,7 +10,33 @@ pkg load optim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
+f = [10, 2*NG]';
+
+##x = [x1, x2]
+x0 = [1, 1]';
+
+## -(Ax)<=(-b) pois é um maior/igual
+A = [-0.3, -0.2;
+     -0.2, -0.25;
+     -0.25, -0.3
+     1, 0
+     0, 1];
+b = [-15;
+     -10;
+     -12;
+     20;
+     50];
+
+## Aeq e beq nulos, pois não há igualdade
+Aeq=[];
+beq=[];
+
+##limites dos valores de x
+x_min_valores = zeros(2,1);
+x_max_valores = [20; 50];
+[x_star, fval_star] = linprog(f, A, b, Aeq, beq, lb=x_min_valores, ub=x_max_valores)
+
+custo = f'*x_star
 
 
 % mantenha essas duas linhas finais
