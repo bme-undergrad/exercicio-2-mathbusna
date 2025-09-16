fa74912ea4b8fc70b6772bfbb160b2ed3c16aed7
mathbusna <matheus.busnardo@gmail.com>
2025-09-16 11:41:00 -0300

    Update exercicio2.m

--- exercicio2.m
+++ exercicio2.m
@@ -1,15 +1,6 @@
 function [blendA, blendB] = exercicio2(NG)
 
-% NG: 1
-
-% nao alterar: inicio
-es = 1;
-imax = 20;
-pkg load optim
-% nao alterar: fim
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+##f = @(x)(10*x(1)+2*1*x(2));
 f = [10, 2*NG]';
 
 ##x = [x1, x2]
@@ -18,14 +9,10 @@ x0 = [1, 1]';
 ## -(Ax)<=(-b) pois é um maior/igual
 A = [-0.3, -0.2;
      -0.2, -0.25;
-     -0.25, -0.3
-     1, 0
-     0, 1];
+     -0.25, -0.3]
 b = [-15;
      -10;
-     -12;
-     20;
-     50];
+     -12]
 
 ## Aeq e beq nulos, pois não há igualdade
 Aeq=[];
@@ -34,15 +21,12 @@ beq=[];
 ##limites dos valores de x
 x_min_valores = zeros(2,1);
 x_max_valores = [20; 50];
-[x_star, fval_star] = linprog(f, A, b, Aeq, beq, lb=x_min_valores, ub=x_max_valores)
+[x_star, fval_star] = linprog(f, A, b, Aeq, beq, lb=x_min_valores, ub=x_max_valores);
 
 custo = f'*x_star
 
-
-% mantenha essas duas linhas finais
-blendA = x(1);
-blendB = x(2);
-
+blendA=x_star(1);
+blendB=x_star(2);
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endfunction
